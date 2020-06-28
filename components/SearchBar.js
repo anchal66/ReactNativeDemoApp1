87b0d86949261af3cf83b43fae4773523563e1f0
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-28 17:39:57 +0530

    Adding Icons and styling search bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
+import { Feather } from '@expo/vector-icons'; 
 
 export default SearchBar = () => {
-    return <View style={styles.background}>
-        <Text>In Bar</Text>
+    return <View style={styles.backgroundStyle}>
+        <Feather name="search" size={30} color="black" />
+        <TextInput style={styles.inputStyle} placeholder="Search"/>
     </View>
 }
 const styles = StyleSheet.create({
-    background:{
+    backgroundStyle:{
         backgroundColor: '#F0EEEE',
         height:50,
         borderRadius: 5,
-        marginHorizontal: 15
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+    inputStyle:{
+        borderColor: 'black',
+        borderWidth: 1,
+        flex: 1
     }
 })
