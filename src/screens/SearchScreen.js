967072daca1fba40ad49866fd32521df26ed98c8
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-28 17:20:20 +0530

    Adding Navigation - npm i react-navigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {View, StyleSheet, Text} from 'react-native';
+
+export default SearchScreen =() =>{
+    return <Text>In Serach</Text>
+}
+const styles = StyleSheet.create({})
