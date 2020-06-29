48a343916aab46eec92d6edcb4d0ce6bf41becb6
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-29 12:49:40 +0530

    Showing some addtional info

--- components/ResultDetails.js
+++ components/ResultDetails.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View, StyleSheet, Text, TextInput } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Image } from 'react-native';
 
 export default ResultDetails = (props) => {
     return <View>
-        <Text>{props.restruant.name}</Text>
+        <Image style={styles.imageStyle} source={{uri: props.restruant.image_url}}/>
+        <Text style={styles.nameStyle}>{props.restruant.name}</Text>
+        <Text>{props.restruant.rating} Stars, {props.restruant.review_count} Reviews</Text>
     </View>
 }
 const styles = StyleSheet.create({
+    imageStyle:{
+        width: 250,
+        height: 120,
+        borderRadius: 4
+    },
+    nameStyle:{
+        fontWeight: 'bold'
+    }
 })
