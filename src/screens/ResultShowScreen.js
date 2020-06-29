51d7369fb4906314142d2ed039c6a2b12c1e6a84
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-29 17:12:45 +0530

    Fetching sinlge restrn from API

--- src/screens/ResultShowScreen.js
+++ src/screens/ResultShowScreen.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, Text } from 'react-native';
+import yelp from '../api/yelp';
 
 export default ResultShowScreen = ({navigation}) => {
-    const id = navigation.getParam('id')
-    console.log('id', id)
+    const id = navigation.getParam('id');
+    const [result, setResult] = useState(null);
+    console.log('rest',result)
+
+    const getRestraunt =async (id) =>{
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    }
+
+    useEffect(()=>{
+        getRestraunt(id);
+    },[]);
+
     return <Text>Testing</Text>
 }
 const styles = StyleSheet.create({});
